b427b8038ef7a392ad92f5a054617166ba185bac
goldsborough <peter@goldsborough.me>
2018-12-10 23:06:13 -0800

    Initial Commit

---
+++ GoogleCloudVisionOCRExample/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  GoogleCloudVisionOCRExample
+//
+//  Created by Peter Goldsborough on 10/12/2018.
+//  Copyright © 2018 Peter Goldsborough. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view, typically from a nib.
+  }
+
+
+}
+
